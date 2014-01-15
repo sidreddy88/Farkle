bf4cc9ac05e3f65f8edf01c12862937a4100828a
Siddharth Sukumar <sidreddy88@gmail.com>
2014-01-15 15:11:01 -0600

    Initial Commit

---
+++ Farkle/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Farkle
+//
+//  Created by Siddharth Sukumar on 1/15/14.
+//  Copyright (c) 2014 Siddharth Sukumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Farkle/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Farkle
+//
+//  Created by Siddharth Sukumar on 1/15/14.
+//  Copyright (c) 2014 Siddharth Sukumar. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
